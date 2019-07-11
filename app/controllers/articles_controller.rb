282d0b9854e47e4a51c0431386805c12bb2b0807
Rails Course <c.chick@unsw.edu.au>
2019-07-11 10:05:33 +1000

    Fix create, add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,27 @@
 class ArticlesController < ApplicationController
 
+  # go to new.html.erb
   def new
     @article = Article.new
   end
 
   def create
-    #
-    render plain: params[:article].inspect
+    # send plain text - with no markup at all - back to the browser by using the :plain option to render:
+    # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
+    # @article.save
+    # redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)   # go to 'show' action
+    else
+      # if have an error
+      render 'new'    # go to 'new' action
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 # in Rail4, use class Article < ActiveRecord::Base
-# Section 79
+# Section 79 validation to control save to db
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum:50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
