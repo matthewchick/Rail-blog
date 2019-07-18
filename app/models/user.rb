245bfe4dcfc3cc1dc5a61143428b46a610a26661
Matthew Chick <c.chick@unsw.edu.au>
2019-07-18 10:30:10 +1000

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  #https://guides.rubyonrails.org/
+  validates :username, presence: true,
+        uniqueness: { case_sensitive: false },
+        length: { minimum: 3, maximum: 25 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # rubular.com
+  validates :email, presence: true, length: { maximum: 105 },
+        uniqueness: { case_sensitive: false },
+        format: { with: VALID_EMAIL_REGEX }
 
 end
